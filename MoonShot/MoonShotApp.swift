6f57decf190a239238f928b8df849e2fdb0f5984
Braian <villasantibraian@icloud.com>
2022-10-07 04:10:46 -0300

    Initial Commit

---
+++ MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by Braian Villasanti on 07/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
